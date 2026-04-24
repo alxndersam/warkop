fe02462cd51d062c6fa524a2dfa53668a4b86679
alxndersam <samsammysam007@gmail.com>
2026-04-24 19:16:38 +0700

    Update Firebase configuration details

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhVjXJX4Wmi6sTFT2-Lm8vUG_QB-nV6Fk",
-  authDomain: "coffeeshop-61380.firebaseapp.com",
-  projectId: "coffeeshop-61380",
-  storageBucket: "coffeeshop-61380.firebasestorage.app",
-  messagingSenderId: "545529883528",
-  appId: "1:545529883528:web:9069b798b73b253ac3683d"
+  apiKey: "ISI_PUNYA_LO",
+  authDomain: "ISI_PUNYA_LO",
+  projectId: "ISI_PUNYA_LO",
+  storageBucket: "ISI_PUNYA_LO",
+  messagingSenderId: "ISI_PUNYA_LO",
+  appId: "ISI_PUNYA_LO"
 };
 
 const app = initializeApp(firebaseConfig);
