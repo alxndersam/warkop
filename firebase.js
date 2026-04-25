e560526f41d1100663c99b7e8904075e7c141190
alxndersam <samsammysam007@gmail.com>
2026-04-25 20:03:05 +0700

    Update Firebase configuration with new settings

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
+// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // config lo
+  apiKey: "AIzaSyDIog8cGM6HWbSj-02rUOh9Wn9JahPix2U",
+  authDomain: "wanmoein-kopi.firebaseapp.com",
+  projectId: "wanmoein-kopi",
+  storageBucket: "wanmoein-kopi.firebasestorage.app",
+  messagingSenderId: "434124582679",
+  appId: "1:434124582679:web:952776c6a04e6390981152",
+  measurementId: "G-0Q7YFY5NLT"
 };
 
 const app = initializeApp(firebaseConfig);
